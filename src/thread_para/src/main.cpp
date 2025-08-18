da535ef2a403588ee719ffc3c450a4299424df55
handabao <18862963156@163.com>
2025-08-18 16:59:07 +0800

    C++11 14 17 20 多线程从原理到线程池实战【486376】全局函数作为线程入口分析参数传递内存操作

---
+++ src/thread_para/src/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <thread>
+#include <print>
+
+void ThreadMain(float param_first, const std::string &param_second)
+{
+    std::cout << "begin sub thread main " << std::this_thread::get_id() << std::endl;
+    {
+        std::print("param_first = {}, param_second = {};\n", param_first, param_second);
+    }
+    std::cout << "end sub thread main " << std::this_thread::get_id() << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+
+    {
+        float       p1 = 12.12f;
+        std::string p2 = "test thread param";
+        std::thread th;
+        {
+            th = std::thread(ThreadMain, p1, p2);
+        }
+        th.join();
+    }
+
+
+    getchar();
+    return 0;
+}
