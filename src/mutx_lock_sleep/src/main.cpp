3297a1fa49f33985561743633263d5a0d1c13281
handabao <18862963156@163.com>
2025-08-19 17:30:49 +0800

    多线程切换(要给时间(微秒级) 切换)

---
+++ src/mutx_lock_sleep/src/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+#include <print>
+
+static std::mutex mutx;
+
+void ThreadMainMux(int i)
+{
+    for (;;)
+    {
+        mutx.lock();
+        std::cout << i << "[in]" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        mutx.unlock();
+        /// unlock(释放资源) 之后， 直接 lock， 没有时间切换到另一个线程，所有大概率会还是会自己获得
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); /// 给时间切换
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        std::thread(ThreadMainMux, i).detach();
+    }
+
+    getchar();
+    return 0;
+}
