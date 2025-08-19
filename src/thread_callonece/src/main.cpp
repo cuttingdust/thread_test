2752f804deb08307530e5984c3df26b994baac5e
handabao <18862963156@163.com>
2025-08-19 15:05:10 +0800

    37call_once 多线程调用函数只进入一次

---
+++ src/thread_callonece/src/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <thread>
+#include <print>
+
+void SystemInit()
+{
+    std::cout << "Call SystemInit" << std::endl;
+}
+
+void SystemInitOne()
+{
+    static std::once_flag flag;
+    std::call_once(flag, SystemInit);
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+
+    SystemInitOne();
+    SystemInitOne();
+    for (int i = 0; i < 3; i++)
+    {
+        std::thread th(SystemInitOne);
+        th.detach();
+    }
+
+    getchar();
+
+    return 0;
+}
