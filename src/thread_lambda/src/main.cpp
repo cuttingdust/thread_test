43c3b07ff1efe0557d12a7a2190d35b02bbd0b52
handabao <18862963156@163.com>
2025-08-19 15:11:07 +0800

    6lambda临时函数作为线程入口

---
+++ src/thread_lambda/src/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <thread>
+#include <print>
+
+/// test lambda thread
+class TestLambda
+{
+public:
+    void start()
+    {
+        std::thread th([this]() {
+            std::cout << "name = " << name << std::endl; });
+        th.join();
+    }
+
+    std::string name = "test lambda";
+};
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+
+    std::thread th([](int i) { std::cout << "test lmbda " << i << std::endl; }, 123);
+    th.join();
+
+    TestLambda test;
+    test.start();
+
+    getchar();
+    return 0;
+}
