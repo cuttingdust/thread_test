c5c7acfd36812660de6d151c8220e11f901a12a7
handabao <18862963156@163.com>
2025-08-20 17:04:59 +0800

    update

--- src/usageMutx_lockguard/src/main.cpp
+++ src/usageMutx_lockguard/src/main.cpp
@@ -9,24 +9,24 @@ void TestLockGuard(int i)
 {
     mtx.lock();
     {
-        /// 已经拥有锁，不lock
+        ///已经拥有锁，不lock
         std::lock_guard<std::mutex> lock(mtx, std::adopt_lock);
         /// 结束释放锁
     }
 
     {
-        std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard<std::mutex> lg(mtx);
         std::cout << "begin thread " << i << std::endl;
     }
 
     for (;;)
     {
         {
-            std::lock_guard<std::mutex> lock(mtx);
+            std::lock_guard<std::mutex> lg(mtx);
             std::cout << "In " << i << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
