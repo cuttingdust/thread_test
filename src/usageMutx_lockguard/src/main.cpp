a75f05be458490ebf36f1b9f0507b90efc75e0da
handabao <18862963156@163.com>
2025-08-20 15:59:17 +0800

    13c++11RAII控制锁lock_guard

---
+++ src/usageMutx_lockguard/src/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+#include <print>
+
+static std::mutex mtx;
+
+void TestLockGuard(int i)
+{
+    mtx.lock();
+    {
+        /// 已经拥有锁，不lock
+        std::lock_guard<std::mutex> lock(mtx, std::adopt_lock);
+        /// 结束释放锁
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "begin thread " << i << std::endl;
+    }
+
+    for (;;)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::cout << "In " << i << std::endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        std::thread(TestLockGuard, i).detach();
+    }
+
+
+    getchar();
+    return 0;
+}
