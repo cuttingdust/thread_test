e6a94811f585609b06e683ae783eb0d9938b0058
handabao <18862963156@163.com>
2025-08-21 11:06:30 +0800

    15 C++14shared_lock共享锁包装器

---
+++ src/usageMutx_shared_lock/src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <shared_mutex>
+#include <thread>
+#include <print>
+
+/// 共享锁
+static std::shared_timed_mutex st_mtx;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+    {
+        {
+            /// 读取锁 共享锁
+            std::shared_lock<std::shared_timed_mutex> sl(st_mtx);
+            std::cout << "read data" << std::endl;
+            /// 退出栈区 释放共享锁
+        }
+
+        //////////////////////////////////////////////////////////////////
+
+        {
+            /// 写入锁 互斥锁
+            std::unique_lock<std::shared_timed_mutex> ul(st_mtx);   
+            std::cout << "write data" << std::endl;
+        }
+    }
+    getchar();
+    return 0;
+}
