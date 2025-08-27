7addbdf2e898a533392f70d6b1626883f0941186
handabao <18862963156@163.com>
2025-08-27 14:52:02 +0800

    22packaged_task 异步调用函数打包

---
+++ src/async_packaged_task/src/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <future>
+#include <iostream>
+
+
+std::string TestPack(int index)
+{
+    std::cout << "begin Test Pack " << index << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    return "Test Pack return";
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "main thread ID " << std::this_thread::get_id() << std::endl;
+    {
+        std::packaged_task<std::string(int)> task(TestPack);
+        auto                                 result = task.get_future();
+        // task(100);
+        std::thread(std::move(task), 101).detach();
+
+        std::cout << "begin result get" << std::endl;
+        std::cout << "result get " << result.get() << std::endl;
+    }
+    getchar();
+    return 0;
+}
